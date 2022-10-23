27a022a07f507c0fc028a8680fe922a58121bf05
Sid-the-Developer <swright@gatech.edu>
2022-10-23 06:12:04 -0400

    Fade scale transitions added to home screen.

--- OMagotchi/lib/home.dart
+++ OMagotchi/lib/home.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:omagotchi/omagotchi.dart';
 import 'package:omagotchi/settings.dart';
 import 'package:omagotchi/stats.dart';
@@ -33,13 +35,18 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late Omagotchi avatar;
   late SharedPreferences prefs;
   bool firstTime = true;
   Future<bool> _avatarFuture = Future.value(false);
-  // allows for closure of drawer programmatically when selecting a new OMi
+
+  /// allows for closure of drawer programmatically when selecting a new OMi
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  /// controller that starts fade scale animation for image and task view
+  late final AnimationController _fadeScaleController =
+      AnimationController(vsync: this, duration: const Duration(milliseconds: 300));
 
   double avatarPosition = 0;
   Curve positionCurve = Curves.easeOutSine;
@@ -77,13 +84,15 @@ class _MyHomePageState extends State<MyHomePage> {
       // runs the choose avatar process, which immediately returns true if avater not null
       WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
         setState(() {
-          _avatarFuture = !firstTime ? Future.value(true) : chooseAvatar(context);
+          _avatarFuture =
+              !firstTime ? Future.value(true) : chooseAvatar(context);
+        });
+        Future.delayed(const Duration(milliseconds: 200), () {
+          _fadeScaleController.forward();
         });
       });
     });
-
-
-    }
+  }
 
   Future<bool> chooseAvatar(BuildContext context) {
     if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
@@ -92,12 +101,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return showDialog(
         barrierDismissible: !firstTime,
         context: context,
-        builder: (context) =>
-            SimpleDialog(
+        builder: (context) => SimpleDialog(
+          title: const Center(child: Text('Choose your OMagotchi!')),
               children: List.generate(
                   widget.omis.length,
-                      (index) =>
-                      SimpleDialogOption(
+                  (index) => SimpleDialogOption(
                         child: Column(
                           children: [
                             SizedBox(
@@ -121,144 +129,159 @@ class _MyHomePageState extends State<MyHomePage> {
             )).then((value) => true);
   }
 
-
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     // WillPopScope calls _save before closing app to save data
-      return Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/background-1.png'),
-                fit: BoxFit.cover)),
-        child: Scaffold(
-          key: _scaffoldKey,
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background-1.png'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        key: _scaffoldKey,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
             // Here we take the value from the MyHomePage object that was created by
             // the App.build method, and use it to set our appbar title.
-              shape: const BeveledRectangleBorder(),
-              elevation: 0,
-              iconTheme: const IconThemeData(color: Colors.black),
-              backgroundColor: Colors.transparent),
-          drawer: Drawer(
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(50),
-                    bottomRight: Radius.circular(50))),
-            child: ListView(
-              children: [
-                DrawerHeader(
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Text(
-                        'OMagotchi',
-                        style: Theme.of(context).textTheme.displaySmall,
-                      ),
-                    )),
-                ListTile(
-                  title: Text('Choose new OMi'),
-                  onTap: () => chooseAvatar(context),
+            shape: const BeveledRectangleBorder(),
+            elevation: 0,
+            iconTheme: const IconThemeData(color: Colors.black),
+            backgroundColor: Colors.transparent),
+        drawer: Drawer(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50))),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'OMagotchi',
+                  style: Theme.of(context).textTheme.displaySmall,
                 ),
-                const ListTile(
-                  title: Text('Completed Tasks'),
-                ),
-                ListTile(
-                  title: const Text('Settings'),
-                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const SettingsPage())),
-                ),
-                const ListTile(
-                  title: Text('More features coming soon'),
-                )
-              ],
-            ),
+              )),
+              ListTile(
+                title: const Text('Choose new OMi'),
+                onTap: () => chooseAvatar(context),
+              ),
+              const ListTile(
+                title: Text('Completed Tasks'),
+              ),
+              ListTile(
+                title: const Text('Settings'),
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const SettingsPage())),
+              ),
+              const ListTile(
+                title: Text('More features coming soon'),
+              )
+            ],
           ),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: FutureBuilder<bool>(
-                  future: _avatarFuture,
-                  builder: (context, finished) => (!(finished.data ?? false)) ? CircularProgressIndicator() : Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Spacer(
-                        flex: 2,
-                      ),
-                      Text(
-                        '${avatar.name} is feeling ${avatar.mood}', style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                      GestureDetector(
-                        onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => StatsPage(avatar: avatar))),
-                        onDoubleTap: () => Timer.periodic(
-                            const Duration(milliseconds: 200), (timer) {
-                          setState(() {
-                            positionCurve = Curves.easeOutSine;
-                            avatarPosition = avatarPosition == 100 ? 0 : 100;
-                          });
-                          if (timer.tick == 6) {
-                            timer.cancel();
-                            setState(() {
-                              avatar.stats.happiness += 5;
-                              avatar.setMood();
-                              prefs.setString('avatar', jsonEncode(avatar));
-                            });
-                          } // stop jumping after 3 jumps
-                        }),
-                        onLongPress: () => Timer.periodic(
-                            const Duration(milliseconds: 100), (timer) {
-                          setState(() {
-                            positionCurve = Curves.bounceInOut;
-                            avatarPosition = avatarPosition == 100 ? 0 : 100;
-                          });
-                          if (timer.tick == 6) {
-                            setState(() {
-                              avatar.stats.alertness -= 5;
-                              avatar.stats.hunger += 2;
-                              prefs.setString('avatar', jsonEncode(avatar));
-                            });
-                            timer.cancel();
-                          } // stop jumping after 3 jumps
-                        }),
-                        child: SizedBox(
-                          height: 300,
-                          width: 300,
-                          child: Hero(
-                            tag: 'omagotchi',
-                            child: Stack(alignment: Alignment.center, children: [
-                              AnimatedPositioned(
-                                height: 250,
-                                width: 250,
-                                bottom: avatarPosition,
-                                curve: positionCurve,
-                                duration: const Duration(milliseconds: 200),
-                                child: Image.asset(
-                                  avatar.image,
-                                  fit: BoxFit.contain,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: FutureBuilder<bool>(
+                future: _avatarFuture,
+                builder: (context, finished) => (!(finished.data ?? false))
+                    ? const CircularProgressIndicator()
+                    : FadeScaleTransition(
+                        animation: _fadeScaleController,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Spacer(
+                              flex: 2,
+                            ),
+                            Text(
+                              '${avatar.name} is feeling ${avatar.mood}',
+                              style: Theme.of(context).textTheme.labelLarge,
+                            ),
+                            GestureDetector(
+                              onTap: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          StatsPage(avatar: avatar))),
+                              onDoubleTap: () => Timer.periodic(
+                                  const Duration(milliseconds: 200), (timer) {
+                                setState(() {
+                                  positionCurve = Curves.easeOutSine;
+                                  avatarPosition =
+                                      avatarPosition == 100 ? 0 : 100;
+                                });
+                                if (timer.tick == 6) {
+                                  timer.cancel();
+                                  setState(() {
+                                    avatar.stats.happiness += 5;
+                                    avatar.setMood();
+                                    prefs.setString(
+                                        'avatar', jsonEncode(avatar));
+                                  });
+                                } // stop jumping after 3 jumps
+                              }),
+                              onLongPress: () => Timer.periodic(
+                                  const Duration(milliseconds: 100), (timer) {
+                                setState(() {
+                                  positionCurve = Curves.bounceInOut;
+                                  avatarPosition =
+                                      avatarPosition == 100 ? 0 : 100;
+                                });
+                                if (timer.tick == 6) {
+                                  setState(() {
+                                    avatar.stats.alertness -= 5;
+                                    avatar.stats.hunger += 2;
+                                    prefs.setString(
+                                        'avatar', jsonEncode(avatar));
+                                  });
+                                  timer.cancel();
+                                } // stop jumping after 3 jumps
+                              }),
+                              child: SizedBox(
+                                height: 300,
+                                width: 300,
+                                child: Hero(
+                                  tag: 'omagotchi',
+                                  child: Stack(
+                                      alignment: Alignment.center,
+                                      children: [
+                                        AnimatedPositioned(
+                                          height: 250,
+                                          width: 250,
+                                          bottom: avatarPosition,
+                                          curve: positionCurve,
+                                          duration:
+                                              const Duration(milliseconds: 200),
+                                          child: Image.asset(
+                                            avatar.image,
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ]),
                                 ),
                               ),
-                            ]),
-                          ),
+                            ),
+                            const Spacer(),
+                            Expanded(
+                              flex: 3,
+                              child: ListView.builder(
+                                  clipBehavior: Clip.none,
+                                  physics: const BouncingScrollPhysics(),
+                                  itemCount: tasks.length,
+                                  itemBuilder: (context, index) =>
+                                      tasks[index]),
+                            )
+                          ],
                         ),
                       ),
-                      const Spacer(),
-                      Expanded(
-                        flex: 3,
-                        child: ListView.builder(
-                            clipBehavior: Clip.none,
-                            physics: const BouncingScrollPhysics(),
-                            itemCount: tasks.length,
-                            itemBuilder: (context, index) => tasks[index]),
-                      )
-                    ],
-                  ),
-                ),
               ),
             ),
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
